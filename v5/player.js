c6eef7143b1ca3dbb5c8bf13ed39185b5b0035b7
sammylevin10 <sammylevin10@gmail.com>
2021-11-10 02:49:55 -0500

    v5: production

--- v5/player.js
+++ v5/player.js
@@ -1,3 +1,11 @@
+// Duet - player.js
+// Sahana Sripadanna and Sammy Levin 2021
+// Handles in-browser playback and music visualization via p5
+
+// API Authorization flow is based off of https://gist.github.com/eamonnbell/84e3463e964eeae87f59699c7e711f1b
+// SDK web playback is based off of https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+
+// State variables which are referenced by the visual and playback icons
 let playing = false;
 let currentTrackId = "11dFghVXANMlKmJXsNCbNl";
 let currentTrackFeatures = {};
@@ -11,15 +19,18 @@ let ACCESS_TOKEN;
 
 window.onSpotifyWebPlaybackSDKReady = () => {
   startWebPlaybackSDK();
+  // Try a test call to get track features on the placeholder track id
   fetchFeaturesInformation(features_endpoint);
 };
 
+// Extract the access token from the URL
 function getCurrentQueryParameters(delimiter = "#") {
   const currentLocation = String(window.location).split(delimiter)[1];
   const params = new URLSearchParams(currentLocation);
   return params;
 }
 
+// Update the song and artist p tags in HTML
 function updateSongInformation(name, artist) {
   const nameSelect = document.querySelector("#name");
   const artistSelect = document.querySelector("#artist");
@@ -27,6 +38,7 @@ function updateSongInformation(name, artist) {
   artistSelect.textContent = artist;
 }
 
+// Query the API for track features
 function fetchFeaturesInformation(endpoint) {
   const currentQueryParameters = getCurrentQueryParameters("#");
   ACCESS_TOKEN = currentQueryParameters.get("access_token");
@@ -38,12 +50,10 @@ function fetchFeaturesInformation(endpoint) {
   };
   fetch(endpoint, fetchOptions)
     .then(function (response) {
-      // console.log(response);
       return response.json();
     })
     .then(function (json) {
       currentTrackFeatures = json;
-      console.log("Current track features");
       console.log(currentTrackFeatures);
     })
     .catch(function (error) {
@@ -96,6 +106,7 @@ function startWebPlaybackSDK() {
     console.error(message);
   });
 
+  // Play button
   document.getElementById("togglePlay").onclick = function () {
     player.togglePlay().then(() => {
       playing = !playing;
@@ -113,6 +124,7 @@ function startWebPlaybackSDK() {
       updateCurrentTrack(player);
     });
   };
+  // Next button
   document.getElementById("nextTrack").onclick = function () {
     playing = true;
     updateButton();
@@ -125,6 +137,7 @@ function startWebPlaybackSDK() {
       updateCurrentTrack(player);
     });
   };
+  // Previous button
   document.getElementById("previousTrack").onclick = function () {
     playing = true;
     updateButton();
@@ -139,6 +152,7 @@ function startWebPlaybackSDK() {
   };
 }
 
+// Refresh elements on the page based on current track
 function updateCurrentTrack(player) {
   player.getCurrentState().then((state) => {
     currentTrackId = state.track_window.current_track.id;
@@ -171,181 +185,4 @@ buildButton.addEventListener("click", hideModal);
 
 function hideModal() {
   document.querySelector(".center-wrapper").style.display = "none";
-}
-
-// p5 stuff
-
-// Sound
-let mic;
-let fft, peakDetect;
-var ellipseWidth = 0;
-
-let blooms = [];
-
-function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.parent("#canvas-container");
-  background(0);
-  noStroke();
-  mic = new p5.AudioIn();
-  mic.start();
-  fft = new p5.FFT();
-  fft.setInput(mic);
-  peakDetect = new p5.PeakDetect();
-  colorMode(HSB, 100, 100, 100, 100);
-}
-
-function draw() {
-  let energy = currentTrackFeatures.energy;
-  let acousticness = currentTrackFeatures.acousticness;
-  background(0, 0, 0, 5);
-  micLevel = mic.getLevel();
-  // console.log(micLevel);
-  // testBloom = new Bloom1(width / 2, height / 2, 14, micLevel * 100);
-  // testBloom.moveAndDisplay();
-
-  let spectrum = fft.analyze();
-
-  stroke(255, 255, 255, 20);
-  // strokeWeight(0.5);
-  noFill();
-  // if (playing) {
-  //   beginShape();
-  //   for (i = 0; i < spectrum.length; i++) {
-  //     vertex(
-  //       map(i, 0, spectrum.length, 0, width),
-  //       map(spectrum[i], 0, 255, height - 100, 0)
-  //     );
-  //   }
-  //   endShape();
-  // }
-
-  peakDetect.update(fft);
-  // console.log(peakDetect.isDetected);
-
-  if (peakDetect.isDetected) {
-    background(map(acousticness, 0, 1, 15, 85), 70, 90, 5);
-    let bass;
-    bass = map(spectrum[250], 0, 255, 0, 1);
-    temp = new Bloom1(
-      random(width / 5, (width * 4) / 5),
-      random(height / 5, (height * 4) / 5),
-      floor(random(5, 10)),
-      floor(400 * map(bass, 0, 1, 0, 1)),
-      map(acousticness, 0, 1, 10, 90),
-      map(bass, 0, 1, 0, 90),
-      map(energy, 0, 1, -0.1, -0.6)
-    );
-    blooms.push(temp);
-  }
-
-  for (let i = 0; i < blooms.length; i++) {
-    // console.log("displaying");
-    blooms[i].moveAndDisplay();
-    if (blooms[i].isDead()) {
-      blooms.splice(i, 1);
-      // console.log("deleting " + i);
-      i--;
-    }
-  }
-}
-
-class Bloom1 {
-  // x position, y position, rotation count, size
-  constructor(x, y, c, s, hue, saturation, acceleration) {
-    this.x = x;
-    this.y = y;
-    this.c = c;
-    this.s = s;
-    this.hue = hue;
-    this.saturation = saturation;
-    this.velocity = 5;
-    this.acceleration = acceleration;
-    this.rotation = 0;
-    this.rotationVelocity = random(0.2, 1);
-    this.generateColors(hue);
-    // this.startColor = color(this.hue, this.saturation, 65, 30);
-    // this.endColor = color(random(100), this.saturation, 65, 60);
-    this.colorPosition = 0;
-    this.particles = [];
-    for (let i = 0; i < random(10, 20); i++) {
-      let temp = new Particle1(x, y, s / 5, this.startColor, this.endColor);
-      this.particles.push(temp);
-    }
-  }
-  generateColors(hue) {
-    let start = hue - random(10);
-    let end = hue + random(10);
-    if (start < 0) {
-      start = 100 + start;
-    }
-    if (end > 100) {
-      end = end - 100;
-    }
-    this.startColor = color(min(start, end), this.saturation, 65, 30);
-    this.endColor = color(max(start, end), this.saturation, 65, 60);
-  }
-  moveAndDisplay() {
-    this.colorPosition += 0.05;
-    let color = lerpColor(this.startColor, this.endColor, this.colorPosition);
-    fill(color);
-    this.s += this.velocity;
-    this.velocity += this.acceleration;
-    noStroke();
-    this.rotation += this.rotationVelocity;
-    // rotate(radians(this.rotation));
-    push();
-    translate(this.x, this.y);
-    for (let i = 0; i < this.c; i++) {
-      rotate(radians(360 / this.c + this.rotation));
-      ellipse(this.s, 0, this.s);
-      ellipse(this.s / 2, 0, this.s / 2);
-      ellipse(this.s / 4, 0, this.s / 4);
-    }
-    pop();
-    for (let i = 0; i < this.particles.length; i++) {
-      this.particles[i].moveAndDisplay();
-      if (this.particles[i].isOffScreen()) {
-        this.particles.splice(i, 1);
-        // console.log("deleting " + i);
-        i--;
-      }
-    }
-  }
-  isDead() {
-    return this.s < 1;
-  }
-}
-
-class Particle1 {
-  constructor(x, y, s, startColor, endColor) {
-    this.x = x;
-    this.y = y;
-    this.s = s;
-    this.startColor = endColor;
-    this.endColor = startColor;
-    this.colorPosition = 0;
-    this.xVelocity = random(-15, 15);
-    this.yVelocity = random(-15, 15);
-    this.xAcceleration = this.xVelocity * -1 * 0.01;
-    this.yAcceleration = this.yVelocity * -1 * 0.01;
-  }
-  moveAndDisplay() {
-    this.colorPosition += 0.05;
-    let color = lerpColor(this.startColor, this.endColor, this.colorPosition);
-    fill(color);
-    this.x += this.xVelocity;
-    this.y += this.yVelocity;
-    this.xVelocity += this.xAcceleration;
-    this.yVelocity += this.yAcceleration;
-    ellipse(this.x, this.y, this.s, this.s);
-  }
-  isOffScreen() {
-    let beyondX = this.x > width + 50 || this.x < -50;
-    let beyondY = this.y > height + 50 || this.y < -50;
-    if (beyondX || beyondY) {
-      return true;
-    }
-    return false;
-  }
 }
